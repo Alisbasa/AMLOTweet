a5e70de7b577ca41e522d7f389b518711f574787
AlexCow <a320769@uach.mx>
2022-10-18 11:11:24 -0600

    'acutualizando'

--- tweetApp.py
+++ tweetApp.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import tensorflow as tf
 import pandas as pd
-
+import numpy as np
 import pickle
 import os
 import time
@@ -25,7 +25,7 @@ st.write("""
 """)
 
 tweets = pd.read_csv('tweets.csv', index_col = False)
-tweets.head()
+#tweets.head()
 tweets.drop('id', inplace=True, axis=1)
 tweets.drop('created_at', inplace=True, axis=1)
 tweets.drop('favorite_count', inplace=True, axis=1)
@@ -34,7 +34,7 @@ tweets.drop('retweet_count', inplace=True, axis=1)
 
 tweets.size
 
-tweets.head(1)['text'].values[0]
+#tweets.head(1)['text'].values[0]
 
 #"""Removing links on the tweets"""
 
@@ -72,14 +72,14 @@ for ids in ids_dataset.take(10):
 seq_length = 100
 sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
 
-for seq in sequences.take(1):
-  print(chars_from_ids(seq))
+# for seq in sequences.take(1):
+#   print(chars_from_ids(seq))
 
-def text_from_ids(ids):
-  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+    def text_from_ids(ids):
+    return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
-for seq in sequences.take(5):
-  print(text_from_ids(seq).numpy())
+# for seq in sequences.take(5):
+#   print(text_from_ids(seq).numpy())
 
 #"""Función que regresa un dataset de pares (input, label), donde estos pares el input es una letra de una cadena, y el label la letra que sigue."""
 
@@ -88,13 +88,13 @@ def split_input_target(sequence):
     target_text = sequence[1:]
     return input_text, target_text
 
-split_input_target(list("Tensorflow"))
+#split_input_target(list("Tensorflow"))
 
 dataset = sequences.map(split_input_target)
 
-for input_example, target_example in dataset.take(1):
-    print("Input :", text_from_ids(input_example).numpy())
-    print("Target:", text_from_ids(target_example).numpy())
+# for input_example, target_example in dataset.take(1):
+#     print("Input :", text_from_ids(input_example).numpy())
+#     print("Target:", text_from_ids(target_example).numpy())
 
 #"""Creando batches"""
 
@@ -174,66 +174,66 @@ model =tf.saved_model.load(
 
 # """# Generando Texto"""
 
-# class OneStep(tf.keras.Model):
-#   def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
-#     super().__init__()
-#     self.temperature = temperature
-#     self.model = model
-#     self.chars_from_ids = chars_from_ids
-#     self.ids_from_chars = ids_from_chars
+class OneStep(tf.keras.Model):
+  def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+    super().__init__()
+    self.temperature = temperature
+    self.model = model
+    self.chars_from_ids = chars_from_ids
+    self.ids_from_chars = ids_from_chars
 
-#     # Creamos una mascara para prevenir que se generen carateres "[UNK]".
-#     skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
-#     sparse_mask = tf.SparseTensor(
-#         # ponemos -inf en cada index erroneo.
-#         values=[-float('inf')]*len(skip_ids),
-#         indices=skip_ids,
-#         # hacemos que coincida la forma con el vocabulario
-#         dense_shape=[len(ids_from_chars.get_vocabulary())])
-#     self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+    # Creamos una mascara para prevenir que se generen carateres "[UNK]".
+    skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+    sparse_mask = tf.SparseTensor(
+        # ponemos -inf en cada index erroneo.
+        values=[-float('inf')]*len(skip_ids),
+        indices=skip_ids,
+        # hacemos que coincida la forma con el vocabulario
+        dense_shape=[len(ids_from_chars.get_vocabulary())])
+    self.prediction_mask = tf.sparse.to_dense(sparse_mask)
 
-#   @tf.function
-#   def generate_one_step(self, inputs, states=None):
-#     # strings to token IDs.
-#     input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
-#     input_ids = self.ids_from_chars(input_chars).to_tensor()
+  @tf.function
+  def generate_one_step(self, inputs, states=None):
+    # strings to token IDs.
+    input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+    input_ids = self.ids_from_chars(input_chars).to_tensor()
 
-#     # Run the model.
-#     # predicted_logits.shape is [batch, char, next_char_logits]
-#     predicted_logits, states = self.model(inputs=input_ids, states=states,
-#                                           return_state=True)
-#     # Only use the last prediction.
-#     predicted_logits = predicted_logits[:, -1, :]
-#     predicted_logits = predicted_logits/self.temperature
-#     # Apply the prediction mask: prevent "[UNK]" from being generated.
-#     predicted_logits = predicted_logits + self.prediction_mask
+    # Run the model.
+    # predicted_logits.shape is [batch, char, next_char_logits]
+    predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                          return_state=True)
+    # Only use the last prediction.
+    predicted_logits = predicted_logits[:, -1, :]
+    predicted_logits = predicted_logits/self.temperature
+    # Apply the prediction mask: prevent "[UNK]" from being generated.
+    predicted_logits = predicted_logits + self.prediction_mask
 
-#     # Sample the output logits to generate token IDs.
-#     predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
-#     predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+    # Sample the output logits to generate token IDs.
+    predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+    predicted_ids = tf.squeeze(predicted_ids, axis=-1)
 
-#     # Convert from token ids to characters
-#     predicted_chars = self.chars_from_ids(predicted_ids)
+    # Convert from token ids to characters
+    predicted_chars = self.chars_from_ids(predicted_ids)
 
-#     # Return the characters and model state.
-#     return predicted_chars, states
+    # Return the characters and model state.
+   # return predicted_chars, states
 
-# one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
 
 
 
-# start = time.time()
-# states = None
-# next_char = tf.constant([contexto])
-# result = [next_char]
+start = time.time()
+states = None
+next_char = tf.constant([contexto])
+result = [next_char]
 
-# for n in range(240):
-#   next_char, states = one_step_model.generate_one_step(next_char, states=states)
-#   result.append(next_char)
+for n in range(240):
+  next_char, states = one_step_model.generate_one_step(next_char, states=states)
+  result.append(next_char)
 
-# result = tf.strings.join(result)
-# end = time.time()
-# result2 = result[0].numpy().decode('utf-8'), '\n\n' + '_'*80
-# print(result2)
-# print('\nRun time:', end - start)
-# st.write(result2)
+result = tf.strings.join(result)
+end = time.time()
+result2 = result[0].numpy().decode('utf-8'), '\n\n' + '_'*80
+print(result2)
+print('\nRun time:', end - start)
+st.write(result2)
